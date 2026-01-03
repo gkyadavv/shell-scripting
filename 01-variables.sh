e9455d0330fce4a38444252fb2683fa2c429baf8
gopala <gopalakrishnudu1@gmail.com>
2026-01-03 11:05:12 +0530

    variables topic 02

---
+++ 01-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: HI $PERSON2"
+echo "$PERSON2:: hello $PERSON1"
+echo "$$PERSON1:: how are you $PERSON2"
+echo "$PERSON2:: fine and you"
+echo "$PERSON1:: iam also fine how was the preparartion $PERSON2"
+echo "$PERSON2:: good its going on and how about you"
+echo "$PERSON1:: im also trying my best $PERSON2"
+echo "$PERSON2:: ohh ok $PERSON1"
+echo "$PERSON1:: ok ALL THE BEST AND BEST OF LUCK! $PERSON2"
+echo "$PERSON2:: same to you $PERSON1"
