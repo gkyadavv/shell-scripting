83c6321f2b8df9bffab2b1f03a2f918f880d2fda
gopala <gopalakrishnudu1@gmail.com>
2026-01-03 11:52:21 +0530

    multi variables & -p -sp

---
+++ 03-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#  read variable_name
+#  echo "Enter your name:"
+#  read NAME
+#  echo "Hello, $NAME!"
+#  echo "Enter two values:"
+#  read VAR1 VAR2
+#   echo "You entered $VAR1 and $VAR2"
+
+
+read -p "Enter Your Girl Friend Name::" GF_Name    #-p =prompt
+
+read -sp "Eneter Your GF_number(like password)::" GF_Number 
+
+echo "Enter Two vriables"
+
+read var1 var2
+
+echo " You Entered numbers are $var1 and $var1"
